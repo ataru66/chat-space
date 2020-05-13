1752871c31f3d32d2a0bacd02758ce2860fb6dc9
Ataru Oniki <ataru19980606@gmail.com>
2020-05-13 23:33:56 +0900

    Add valiables for ajax
    後に記述するAjaxでの処理に必要な変数を定義しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,5 +3,7 @@ $(function() {
   // メッセージ送信時の処理
   $('#new_message').on('submit', function(e){
     e.preventDefault();
+    var url = $(this).attr('action');
+    var formData = new FormData(this);
   })
 });
