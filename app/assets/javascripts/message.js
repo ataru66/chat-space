67134e197f80195fecaf97d9003cb66701bc1ced
Ataru Oniki <ataru19980606@gmail.com>
2020-05-13 23:35:58 +0900

    Add ajax process
    ajaxに関する処理を記述しました。
    送信先urlは発火剤になったフォームの送信先、dataにはフォームで入力された情報を代入しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,5 +5,13 @@ $(function() {
     e.preventDefault();
     var url = $(this).attr('action');
     var formData = new FormData(this);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 });
