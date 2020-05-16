465f7c6214f29cf7602e0b5f6c3f621b6ba97ba2
Ataru Oniki <ataru19980606@gmail.com>
2020-05-16 22:37:29 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.consider_all_requests_local = true
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
